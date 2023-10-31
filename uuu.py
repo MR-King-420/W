3d67839430ab2c57d55f573b4fe2a8e06a821c0b
MR-King-420 <148011794+MR-King-420@users.noreply.github.com>
2023-11-01 03:01:43 +0600

    Add files via upload

---
+++ uuu.py
@@ -0,0 +1,35 @@
+import os
+os.system("clear")
+logo =("""\033[1;37m
+   ###    ##          ###    ##     ## #### ##    ##    
+  ## ##   ##         ## ##   ###   ###  ##  ###   ##    
+ ##   ##  ##        ##   ##  #### ####  ##  ####  ##    
+##     ## ##       ##     ## ## ### ##  ##  ## ## ##    
+######### ##       ######### ##     ##  ##  ##  ####    
+##     ## ##       ##     ## ##     ##  ##  ##   ###    
+##     ## ######## ##     ## ##     ## #### ##    ##    
+---------------------------------------------------
+AUTHOR    : MR_Ruhel
+GITHUB    : MrALAMIN156
+FACEBOOK  : MD Fc
+TYPE TYPE : FREE 
+VERSION   : 1.0
+---------------------------------------------------""")                                           
+print (logo)
+print("[1] File clone tutul update")
+print("[2]  File clone ")
+print("[3]   Random clone")
+print("[4]    Contacta Admin ")
+print("[5]     Exit tool")
+
+x = input('Choice: ')
+if x == '1':
+    os.system("1.loging()")
+elif x == '2':
+    os.system("2.loging()")
+elif x == '3':
+    os.system("3.loging()")
+elif x == '4':
+    os.system("xdg-open https://m.me/j/AbZ2my4ItH6-xc-y/")
+elif x == '5':
+    exit()
